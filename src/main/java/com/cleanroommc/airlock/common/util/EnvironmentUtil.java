67de4c65d237fbddb87168d59d7f59e696dceff8
Rongmario <rongmario@protonmail.com>
2022-03-02 00:40:51 +0000

    EnvironmentUtil - logical side & obfuscation state helpers

---
+++ src/main/java/com/cleanroommc/airlock/common/util/EnvironmentUtil.java
@@ -0,0 +1,40 @@
+package com.cleanroommc.airlock.common.util;
+
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
+
+/**
+ * This utility class provides methods related to Minecraft's environment.
+ *
+ * Logical Sides, Obfuscation state etc.
+ */
+public class EnvironmentUtil {
+
+    public static boolean isClient() {
+        return FMLLaunchHandler.side().isClient();
+    }
+
+    public static boolean isServer() {
+        return FMLLaunchHandler.side().isServer();
+    }
+
+    public static boolean isClient(World world) {
+        return world.isRemote;
+    }
+
+    public static boolean isServer(World world) {
+        return !world.isRemote;
+    }
+
+    public static boolean isObfuscated() {
+        return !FMLLaunchHandler.isDeobfuscatedEnvironment();
+    }
+
+    public static boolean isDeobfuscated() {
+        return FMLLaunchHandler.isDeobfuscatedEnvironment();
+    }
+
+    private EnvironmentUtil() { }
+
+}
